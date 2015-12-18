1d4fec070a9e79dfe7109fd90eea4e64f72b2fa1
Maciej Mazur <mmazur@future-processing.com>
2015-12-18 14:29:24 +0100

    more steps

--- test/specs/1-unauthorized/login.js
+++ test/specs/1-unauthorized/login.js
@@ -1,18 +1,15 @@
 describe('when user is not authorized', function () {
-    it('should be possible to login into application', function (done) {
+    it('should be possible to login properly', function (done) {
         browser
             .url('http://localhost:81')
             .setViewportSize({ width: 1280, height: 720 })
-            .title(function(err, res) {
-                expect(res.value).toBe('Scrabble');
-            })
             .waitForExist('[name="loginForm"] [ng-model="user.userName"]')
             .setValue('[name="loginForm"] [ng-model="user.userName"]', 'admincompany1@future-processing.com')
             .waitForExist('[name="loginForm"] [ng-model="user.password"]')
             .setValue('[name="loginForm"] [ng-model="user.password"]', 'P@ssw0rd')
             .waitForEnabled('[name="loginForm"] [type="submit"]')
             .click('[name="loginForm"] [type="submit"]')
-            .waitForVisible('.sc-layout-sidebar', 4000)
+            .waitForVisible('.sc-layout-sidebar')
             .call(done);
     });
 });
